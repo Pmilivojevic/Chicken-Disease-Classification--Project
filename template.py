400bc222166007662c8f8e8e2d632cd93dacdb1b
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2023-12-18 21:39:31 +0100

    User app & prediction added!

--- template.py
+++ template.py
@@ -8,13 +8,6 @@ project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "config/config.yaml",
-    "dvc.yaml",
-    "params.yaml",
-    "requirements.txt",
-    "setup.py",
-    "research/trials.ipnb",
-    "templates/index.html"
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -23,6 +16,13 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipnb",
+    "templates/index.html"
 ]
 
 for file_path in list_of_files:
