526cc3d92ef45023354163f3d1033fd4da9ec30a
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2023-12-18 01:44:42 +0100

    Training added!

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -4,7 +4,8 @@ from cnnClassifier.utils.utils import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (
     DataIngestionConfig,
     PrepareBaseModelConfig,
-    PrepareCallbacksConfig
+    PrepareCallbacksConfig,
+    TrainingConfig
 )
 
 class ConfigurationManager:
@@ -53,8 +54,8 @@ class ConfigurationManager:
         config = self.config.prepare_callbacks
         model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
         create_directories([
+            Path(model_ckpt_dir),
             Path(config.tensorboard_root_log_dir),
-            Path(config.checkpoint_model_filepath)
         ])
 
         prepare_callbacks_config = PrepareCallbacksConfig(
@@ -64,3 +65,23 @@ class ConfigurationManager:
         )
 
         return prepare_callbacks_config
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([Path(training.root_dir)])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCH,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
